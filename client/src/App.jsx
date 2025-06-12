b33d76cd450e53f5f5ac2a9fe40a223dc303fcd3
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-12 13:05:09 -0500

    Basic Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Movies from "./pages/Movies.jsx";
+import MovieDetails from "./pages/MovieDetails.jsx";
+import SeatLayout from "./pages/SeatLayout.jsx";
+import MyBookings from "./pages/MyBookings.jsx";
+import Favorite from "./pages/Favorite.jsx";
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path = "/" element = {<Home/>}/>
+                <Route path = "/movies" element = {<Movies/>}/>
+                <Route path = "/movies/:id" element = {<MovieDetails/>}/>
+                <Route path = "/movies/:id/:date" element = {<SeatLayout/>}/>
+                <Route path = "/my-bookings" element = {<MyBookings/>}/>
+                <Route path = "/favorite" element = {<Favorite/>}/>
+            </Routes>
+        </>
     )
 }
 export default App
---
+++ client/src/pages/SeatLayout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SeatLayout = () => {
+    return (
+        <div>SeatLayout</div>
+    )
+}
+export default SeatLayout
