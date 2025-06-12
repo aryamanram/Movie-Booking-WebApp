1249b4a4ecd93b1038fee4d82e2d92262ad13560
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-12 15:23:54 -0500

    Tailwind Manual download

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 
     return (
         <>
-            <Toaster/>
+            <Toaster />
             {!isAdminRoute && <Navbar/>}
             <Routes>
                 <Route path = "/" element = {<Home/>}/>
