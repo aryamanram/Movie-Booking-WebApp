ec5486f00d45a0063ad73c2a2ff187206013f69b
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-12 14:19:23 -0500

    Minor Admin Handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
+// Packages
 import React from 'react'
 import Navbar from "./components/Navbar.jsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
+
+// Pages
 import Home from "./pages/Home.jsx";
 import Movies from "./pages/Movies.jsx";
 import MovieDetails from "./pages/MovieDetails.jsx";
 import SeatLayout from "./pages/SeatLayout.jsx";
 import MyBookings from "./pages/MyBookings.jsx";
 import Favorite from "./pages/Favorite.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
+
+    const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
     return (
         <>
-            <Navbar/>
+            <Toaster/>
+            {!isAdminRoute && <Navbar/>}
             <Routes>
                 <Route path = "/" element = {<Home/>}/>
                 <Route path = "/movies" element = {<Movies/>}/>
@@ -20,6 +29,7 @@ const App = () => {
                 <Route path = "/my-bookings" element = {<MyBookings/>}/>
                 <Route path = "/favorite" element = {<Favorite/>}/>
             </Routes>
+            {!isAdminRoute && <Footer/>}
         </>
     )
 }
