6c16ddccdae05fbe2a0b6b423976c933ce7f6a72
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-13 15:40:56 -0500

    Movie Cards Dummy
    movie cards added but are currently using dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from "../components/HeroSection.jsx";
+import FeatureSection from "../components/FeatureSection.jsx";
 
 const Home = () => {
     return (
         <>
             <HeroSection/>
+            <FeatureSection/>
         </>
     )
 }
