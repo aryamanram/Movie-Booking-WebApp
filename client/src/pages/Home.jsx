575ec595f10eabea33cbc917efcb097039fd918b
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-13 21:43:05 -0500

    Footer Section
    basic footer with dummy information

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from "../components/HeroSection.jsx";
 import FeatureSection from "../components/FeatureSection.jsx";
+import TrailerSection from "../components/TrailerSection.jsx";
 
 const Home = () => {
     return (
         <>
             <HeroSection/>
             <FeatureSection/>
+            <TrailerSection/>
         </>
     )
 }
