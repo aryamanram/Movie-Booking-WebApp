9d5dfe950e8a0ff76fd19f2cc6c1f3e42fe585a8
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-19 12:22:32 -0400

    Update showController.js

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+// API to fetch now playing movies from TMDB
 export const getNowPlayingMovies = async (req, res) => {
     try {
         const {data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
@@ -9,6 +9,16 @@ export const getNowPlayingMovies = async (req, res) => {
 
         const movies = data.results;
         res.json({success: true, movies: movies})
+    } catch (error) {
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+// API to add new show to the database
+export const addShow = async (req, res) => {
+    try {
+        
     } catch (error) {
         console.error(error);
         res.json({success: false, message: error.message});
