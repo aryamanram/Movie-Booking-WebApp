ab8ed1dad5f43a93e427263bf708c5cac016b15e
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-29 10:15:51 -0500

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import {protectAdmin} from "../middleware/auth.js";
-import {getAllBookings, getAllShows, getDashboardData, isAdmin} from "../controllers/adminController.js";
+import express from "express";
+import { protectAdmin } from "../middleware/auth.js";
+import { getAllBookings, getAllShows, getDashboardData, isAdmin } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
