535b444302e79a44481842711d35d68a476a7866
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-29 10:10:17 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,10 +19,14 @@ import Dashboard from "./pages/admin/Dashboard.jsx";
 import AddShows from "./pages/admin/AddShows.jsx";
 import ListShows from "./pages/admin/ListShows.jsx";
 import ListBookings from "./pages/admin/ListBookings.jsx";
+import {useAppContext} from "./context/AppContext.jsx";
+import {SignIn} from "@clerk/clerk-react";
 
 const App = () => {
 
     const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
+    const { user } = useAppContext()
 
     return (
         <>
@@ -35,7 +39,11 @@ const App = () => {
                 <Route path = "/movies/:id/:date" element = {<SeatLayout/>}/>
                 <Route path = "/my-bookings" element = {<MyBookings/>}/>
                 <Route path = "/favorite" element = {<Favorite/>}/>
-                <Route path = "/admin/*" element = {<Layout/>}>
+                <Route path = "/admin/*" element = {user ? <Layout/> : (
+                    <div className = "min-h-screen flex justify-center items-center">
+                        <SignIn fallbackRedirectUrl = {"/admin"}/>
+                    </div>
+                )}>
                     <Route index element = {<Dashboard/>}/>
                     <Route path = "add-shows" element = {<AddShows/>}/>
                     <Route path = "list-shows" element = {<ListShows/>}/>
