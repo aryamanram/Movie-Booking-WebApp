fa5a00272a211f7feda5112ed0534523f92f95ee
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-29 02:40:28 -0500

    App Context for users and admin

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { AppProvider } from "./context/AppContext.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
         <BrowserRouter>
-            <App />
+            <AppProvider>
+                <App />
+            </AppProvider>
         </BrowserRouter>
     </ClerkProvider>
 )
