96f8b920d3c967997dac76d25dce72400b35968e
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-06-29 10:17:28 -0500

    Routes Check

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import {getFavorites, getUserBookings, updateFavorite} from "../controllers/userController.js";
+import express from "express";
+import { getFavorites, getUserBookings, updateFavorite } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
