ba93274bd70c95ca92aee63e9c45d538bef97ea4
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2025-07-02 13:52:44 -0500

    Dashboard non-dummy
    taken away dummydata from dashboard and connected to shows in database

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -5,10 +5,14 @@ import Loading from "../../components/Loading.jsx";
 import Title from "../../components/admin/Title.jsx";
 import BlurCircle from "../../components/BlurCircle.jsx";
 import {dateFormat} from "../../lib/dateFormat.js";
+import {useAppContext} from "../../context/AppContext.jsx";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
 
     const currency = import.meta.env.VITE_CURRENCY
+
+    const {axios, getToken, user, image_base_url} = useAppContext()
 
     const [dashboardData, setDashboardData] = useState({
         totalBookings: 0,
@@ -27,13 +31,24 @@ const Dashboard = () => {
     ]
 
     const fetchDashboardData = async () => {
-        setDashboardData(dummyDashboardData);
-        setLoading(false);
+        try {
+            const { data } = await axios.get('/api/admin/dashboard', {headers: {Authorization: `Bearer ${await getToken()}`}})
+            if (data.success) {
+                setDashboardData(data.dashboardData)
+                setLoading(false)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error("Error fetching dashboard data", error)
+        }
     };
 
     useEffect(() => {
-        fetchDashboardData();
-    })
+        if (user) {
+            fetchDashboardData();
+        }
+    }, [user])
 
     return !loading ? (
         <>
@@ -59,7 +74,7 @@ const Dashboard = () => {
                 <BlurCircle top = "-100px" left = "-10%"/>
                 {dashboardData.activeShows.map((show) => (
                     <div key = {show._id} className = "w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300">
-                        <img src = {show.movie.poster_path} alt = "" className = "h-60 w-full object-cover"/>
+                        <img src = {image_base_url + show.movie.poster_path} alt = "" className = "h-60 w-full object-cover"/>
                         <p className = "font-medium p-2 truncate">{show.movie.title}</p>
                         <div className = "flex items-center justify-between px-2">
                             <p className = "text-lg font-medium">{currency} {show.showPrice}</p>
